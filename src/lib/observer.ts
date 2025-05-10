40b97d8e47f832d5234a04cdab2b83a1248226d3
Mohammed Taha <gsft22221@gmail.com>
2025-05-10 23:21:50 +0300

    observer: improve location observer

--- src/lib/observer.ts
+++ src/lib/observer.ts
@@ -1,11 +1,15 @@
 export function init_location_observer(location_change_handler: () => void) {
+  let last_location = window.location.href
+
   let observer = new MutationObserver(() => {
     const current_location = window.location.href;
 
-    // if the location changed, run detection process again.
-    if (window.BAHETH_LAST_LOCATION !== current_location) {
+    console.log(last_location, current_location)
+
+    if (last_location !== current_location) {
       location_change_handler();
-      window.BAHETH_LAST_LOCATION = current_location;
+
+      last_location = current_location;
     }
   });
 
